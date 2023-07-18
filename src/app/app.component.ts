de28b7f6f6f028d3fc9a078eb3877b2cace5caf0
HEP CPO Andrew Wang <>
2023-07-17 23:51:51 -0700

    menu bar step1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { FirstTestDemoComponent } from "./first-test-demo/first-test-demo.component";
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { TabMenuModule } from 'primeng/tabmenu';
+// import { FirstTestDemoComponent } from "./first-test-demo/first-test-demo.component";
+import { MenuItem } from 'primeng/api';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [CommonModule, RouterOutlet, FirstTestDemoComponent]
+    imports: [CommonModule, RouterLinkActive, RouterLink, RouterOutlet, TabMenuModule]
 })
 export class AppComponent {
-  title = 'primeng-demo';
+  title = '竹銘醫院';
+
+  tabItems!: MenuItem[];
+
+  ngOnInit(): void {
+    this.fillMenu();
+  }
+
+  fillMenu() {
+    this.tabItems = [
+      {
+        label: 'Home',
+        routerLink: 'home'
+      },
+      {
+        label: 'Patient',
+        routerLink: 'patient',
+      },
+    ]
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'patient',
+    loadComponent: () =>
+      import('./patient/patient.component').then(c => c.PatientComponent)
+  },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
